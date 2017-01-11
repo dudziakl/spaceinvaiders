45ee5bdf9c4094cffe19c1e23c23df96f35fc49f
Valthec <lukaszdudziak@hotmail.com>
2017-01-11 17:41:16 +0100

    Fixed game score that was not reset in new game

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -4,6 +4,7 @@ extends Panel
 
 func _on_NewGameButton_pressed():
 	get_node("/root/game_state").lives = 3
+	get_node("/root/game_state").score = 0
 	get_tree().change_scene("res://scenes/level.tscn")
 
 func _on_QuitButton_pressed():
