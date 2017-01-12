69e7dfe87b981670dc534c3045457334f7ce2b1c
Lukasz Dudziak <lukaszdudziak@hotmail.com>
2017-01-12 22:11:06 +0100

    fixed shot animation

--- scripts/shoot.gd
+++ scripts/shoot.gd
@@ -16,6 +16,8 @@ func _on_visibility_exit_screen():
 	queue_free()
 
 func _on_shoot_area_enter( area ):
+	if (hit):
+		return
 	#Hit an enemy
 	print("Hit: " + area.get_name())
 	if (area.has_method("destroy")):
@@ -28,5 +30,5 @@ func _hit_something():
 	hit = true
 	set_process(false)
 	#Animate splash
-	get_node("AnimatedSprite").play("laser_anim")
-	queue_free()
+	get_node("AnimatedSprite/AnimationPlayer").play("laser_anim")
+	#queue_free()
